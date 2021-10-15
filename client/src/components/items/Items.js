0fba191701d3241017bdd31236ae3720f7fe882d
Collin Gardner <collingardner5@gmail.com>
2021-10-14 19:35:53 -0600

    items showitems and app updates

---
+++ client/src/components/items/Items.js
@@ -0,0 +1,12 @@
+import ItemForm from './ItemForm';
+import ItemList from './ItemList';
+
+const Items = () => (
+  <>
+    <h1>Items</h1>
+    <ItemForm />
+    <ItemList />
+  </>
+)
+
+export default Items;
